1f995834dd6ff4b98e7acecc1df3f3dcb7b999a5
melyman95 <mattlyman12@gmail.com>
2021-08-01 21:15:07 -0700

    Added empty field validation, should work

--- AddMovieForm.cs
+++ AddMovieForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Entity;
+using System.Windows.Forms.Design;
 
 namespace MovieDatabase
 {
@@ -18,25 +19,52 @@ namespace MovieDatabase
             InitializeComponent();
         }
 
+        public bool ValidateMovie()
+        {
+            //validation
+            foreach (var control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    var textbox = (TextBox)control;
+                    if (String.IsNullOrWhiteSpace(textbox.Text))
+                    {
+                        MessageBox.Show("One or more fields are empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textbox.Text = String.Empty;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
         private void addMovieButton_Click(object sender, EventArgs e)
         {
-            //creating database context to add movie object
-            MovieContext context = new MovieContext();
+            //validation
+            if (ValidateMovie())
+            {
+                //creating database context to add movie object
+                MovieContext context = new MovieContext();
 
-            //making movie object and giving it it's properites
-            Movie addedMovie = new Movie();
-            addedMovie.Title = titleBox.Text;
-            addedMovie.releaseYear = int.Parse(releaseYearBox.Text);
-            addedMovie.Rating = ratingBox.Text;
-            addedMovie.Genre = genreBox.Text;
-            addedMovie.Runtime = int.Parse(runtimeBox.Text);
-            addedMovie.Price = decimal.Parse(priceBox.Text);
+                //making movie object and giving it it's properites
+                Movie addedMovie = new Movie();
+                addedMovie.Title = titleBox.Text;
+                addedMovie.releaseYear = int.Parse(releaseYearBox.Text);
+                addedMovie.Rating = ratingBox.Text;
+                addedMovie.Genre = genreBox.Text;
+                addedMovie.Runtime = int.Parse(runtimeBox.Text);
+                addedMovie.Price = decimal.Parse(priceBox.Text);
 
-            //adding movie object to database
-            context.Movies.Add(addedMovie);
-            context.SaveChanges();
+                //adding movie object to database
+                context.Movies.Add(addedMovie);
+                context.SaveChanges();
+
+                string title = titleBox.Text;
 
 
+                DialogResult confirm = MessageBox.Show(title + " was added!");
+            }
+            
 
             
             
